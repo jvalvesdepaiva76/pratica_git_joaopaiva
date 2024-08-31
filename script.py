8f1dbf1683c4dc9cd5ff48167307a54cca826d81
jvaldesdepaiva76 <jv.aldesdepaivagmail.com>
2024-08-31 11:56:31 -0300

    mudando script python

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 from time import sleep
-
-# count from one to ten with a sleep of 1 second between each step
+'''
+Deu mole pra caramba, tremendo vacilão
+Tá todo arrependido, vai comer na minha mão
+Pensou que era o cara, mas não é bem assim
+Agora baba, bobo, vai correr atrás de mim
+'''
 print('Starting count ...')
 
 for i in range(10):
